5f1376d2e58d448f3a8ac02ce7654e27a562ba66
Phignis <baptiste63.foucras.pro@gmail.com>
2021-12-17 15:24:18 +0100

    séparation de BoucleTemporelle en BoucleTemporelle pour la boucle, et GenerateurClick pour la generation des ticks. Observateur devient une interface, et TestClock permet de tester si l'on peut arreter puis reprendre le generateur de click

--- findCookies/src/clock/BoucleTemporelle.java
+++ findCookies/src/clock/BoucleTemporelle.java
@@ -1,14 +1,14 @@
 package clock;
 
+import utile.observateur.Observateur;
 import utile.observateur.Sujet;
 
 /**
  * BoucleTemporelle représente la boucle temporelle présente dans les niveaux du jeu.
- * Il se charge de la fréquence à laquelle la partie métier recoit les notifications de la vue (comme les actions claviers),
- * mais aussi indiquer la fin de la limite de temps de jeu, a laquelle le personne est remis au début du niveau
+ * Il est chargé d'indiquer la fin de la limite de temps de jeu, a laquelle le personne est remise au début du niveau
  */
-public class BoucleTemporelle extends Sujet implements Runnable {
-    // un tick = 16 ms
+public class BoucleTemporelle extends Sujet implements Observateur {
+
     /**
      * nombre de ticks que dure au total la boucle, avant d'indiquer la fin de la boucle temporelle du niveau.
      * @see BoucleTemporelle#BoucleTemporelle(long)
@@ -22,13 +22,6 @@ public class BoucleTemporelle extends Sujet implements Runnable {
      */
     private long nbTicksEcoule;
 
-    /**
-     * thread interne, servant d'effectuer, en parallèle du reste du jeu, la mise en place des ticks de 16ms
-     * @see BoucleTemporelle#BoucleTemporelle(long)
-     * @see BoucleTemporelle#run()
-     */
-    private final Thread threadInterne;
-
 
     ////////////////////////////////
     // CONSTRUCTEURS
@@ -36,14 +29,11 @@ public class BoucleTemporelle extends Sujet implements Runnable {
 
     /**
      * Constructeur de BoucleTemporelle, il lance en interne un nouveau thread (threadInterne), permettant de mettre en place les ticks
-     * @see BoucleTemporelle#threadInterne
      * @param nbSecondesBoucle Le nombre de secondes que dure la boucle temporelle, avant que le niveau soit remis au début de la boucle
      */
     public BoucleTemporelle(long nbSecondesBoucle) {
         this.nbTicksBoucle = (nbSecondesBoucle * 1000 / 16); // conversion des secondes en tick de 16ms
         nbTicksEcoule = 0;
-        threadInterne = new Thread(this);
-        threadInterne.start();
     }
 
     ////////////////////////////////
@@ -85,22 +75,17 @@ public class BoucleTemporelle extends Sujet implements Runnable {
     }
 
     /**
-     * Fonction effectuée par le thread interne à la classe permettant la mise en place du décompte de tick de 16ms, et lance l'event de fin de boucle temporelle
-     * @see BoucleTemporelle#threadInterne
-     * @see BoucleTemporelle#eventFinBoucleTemporelle()
+     * Implémentation de la méthode de Observateur. ici, a chaque notification, on incrémente le nombre de ticks écoulés depuis le début,
+     * et si jamais on a atteint la fin du temps de la boucle, on effectue l'event de fin de boucle, qui notifie tout ce qui est abonné
+     * à la boucle
+     * @see Observateur
+     * @see BoucleTemporelle#setTempsTotalBoucle(long) 
+     * @see BoucleTemporelle#eventFinBoucleTemporelle() 
      */
     @Override
-    public void run() {
-        // boucle du thread
-        while (true) {
-            try {
-                Thread.sleep(16); // on veut un jeu a 60 fps, on permet un check des events toutes les 1/60 de secondes
-                ++nbTicksEcoule;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            eventFinBoucleTemporelle();
-        }
+    public void update() {
+        // update par GenerateurTick
+        ++nbTicksEcoule;
+        eventFinBoucleTemporelle();
     }
 }
