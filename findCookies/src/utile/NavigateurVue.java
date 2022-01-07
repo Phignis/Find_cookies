bf4f92aafeaad8b1d906636e05724e71810ec1f6
Phignis <baptiste63.foucras.pro@gmail.com>
2022-01-07 14:14:05 +0100

    ajout de la documentation de NavigateurVue, catch de l'exception dans TestMenu

--- findCookies/src/utile/NavigateurVue.java
+++ findCookies/src/utile/NavigateurVue.java
@@ -4,10 +4,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 
-
+/**
+ * NavigateurVue permet de changer la scene courante affichée par celle dont le nom est passé en paramètre
+ */
 public class NavigateurVue {
-    private Stage monStage;
 
+    /**
+     * Permet de changer la scène courante affichée pour celle dont le nom est donné en paramètre
+     * @param monStage stage a changer
+     * @param nomScene nom de la scène a afficher
+     * @throws Exception si jamais le nom de scene n'existe pas
+     */
     public void changeScene(Stage monStage, String nomScene) throws Exception {
         nomScene = "/testVues/" + nomScene + ".fxml";
         Parent p = new FXMLLoader(getClass().getResource(nomScene)).load();
