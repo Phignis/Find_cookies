271a7369b7897d8a6c05582330a97bc3b4ea8ecd
Phignis <baptiste63.foucras.pro@gmail.com>
2022-01-19 19:02:45 +0100

    Cell Factory mise en place au sein de ListeNiveauxControleur

--- findCookies/src/controleurs/ListeNiveauxControleur.java
+++ findCookies/src/controleurs/ListeNiveauxControleur.java
@@ -74,7 +74,7 @@ public class ListeNiveauxControleur {
 
     private void genererListeNumNiveaux(FileFilter f) {
         for (File file : trouverListeNiveaux(f)) {
-            try{
+            try {
                 addNumNiveau(Integer.parseInt(file.getName().substring(0, file.getName().lastIndexOf('.'))));
             } catch (NumberFormatException c) {
                 continue; // impossible que cela arrive avec le filtre
@@ -83,9 +83,9 @@ public class ListeNiveauxControleur {
     }
 
     @FXML
-    public void initialize() {
+    private void initialize() {
         listeNumNiveauxAAfficher.itemsProperty().bind(listeNiveauxProperty());
-        listeNumNiveauxAAfficher.setCellFactory(__ -> new ListCell<Integer>() {
+        listeNumNiveauxAAfficher.setCellFactory(__ -> new ListCell<>() {
             @Override
             protected void updateItem(Integer integer, boolean empty) {
                 super.updateItem(integer, empty);
@@ -98,89 +98,4 @@ public class ListeNiveauxControleur {
             }
         });
     }
-
-
-
-
-    /*
-    private InstanciationNiveau instanceNiv; //Récupérer le niveau cliqué
-
-    private List<Niveau> listeNiveaux;
-    //trouver comment instancier une liste du nombre de fichiers du répertoire qui contient les mondes à charger
-
-    @FXML
-    private ListView vueListes;
-    ObservableList ListeObservable = FXCollections.observableArrayList();
-
-    public ListeNiveauxControleur() {
-        try {
-            listeNiveaux = chargerListeNiveaux();
-            if(listeNiveaux != null){
-                for (Niveau n : listeNiveaux) {
-                    System.out.println(n);
-                }
-            }
-
-
-     */
-            //https://webdevdesigner.com/q/customize-listview-in-javafx-with-fxml-103337/
-          //  ListeObservable.setAll(listeNiveaux);
-           // vueListes.setItems(ListeObservable);
-            /*
-            vueListes.setCellFactory(new Callback<ListView<String>, ListCell<String>>()
-            {
-                @Override
-                public ListCell<String> call(ListView<String> vueListes)
-                {
-                    return new vueListesCell();
-                }
-            });
-             */
-    /*
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-    }
-
-    private ArrayList<Niveau> chargerListeNiveaux() throws Exception {
-        ArrayList<Niveau> listeNiveaux = new ArrayList<Niveau>();
-
-        String separateur = File.separator;
-
-        File repertoire = new File("\\\\fichiers" + separateur );
-        if(!repertoire.exists() || !repertoire.isDirectory()){
-            throw new Exception("Le dossier n'est pas accessible");
-        }
-
-        System.out.println(repertoire.getPath());
-
-        var liste = repertoire.list();
-
-
-     */
-        /*
-        URI nomFichier = getClass().getResource("/fichiers/" + numNiveau + ".txt").toURI();
-        if(nomFichier == null){
-            throw new Exception("Le fichier n'est pas accéssible");
-        }
-         */
-/*
-        if (liste != null) {
-            for (int i = 0; i < liste.length; i++) {
-                System.out.println(liste[i]);
-                Niveau n = new Niveau(i);
-                listeNiveaux.add(n);
-            }
-        } else {
-            System.err.println("Nom de repertoire invalide");
-        }
-
-        return listeNiveaux;
-    }
-
-    //verifierNumero.verifierNumeroNiveau(
-
-      ///////////////////////////
-
- */
 }
